92c41c5f74c85b070095a80adb45a576d05acafb
Daigomizuki <daigo3262@gmail.com>
2020-11-21 17:52:27 +0900

    change to sort the message by time

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_chat/constants.dart';
 
 final _firestore = FirebaseFirestore.instance;
+User user = FirebaseAuth.instance.currentUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'char_screen';
@@ -16,8 +17,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  User loggedInUser;
-  User user = FirebaseAuth.instance.currentUser;
+
   String messageText;
 
   // void getMessages() async{
@@ -83,6 +83,7 @@ class _ChatScreenState extends State<ChatScreen> {
                       _firestore.collection('messages').add({
                         'text': messageText,
                         'sender': user.email,
+                        "time": DateTime.now(),
                       });
                     },
                     child: Text(
@@ -112,64 +113,79 @@ class MessagesStream extends StatelessWidget {
               backgroundColor: Colors.lightBlueAccent,
             ),
           );
-        } else {
-          final messages = snapshot.data.docs;
+        }
+          final messages = snapshot.data.docs.reversed;
           List<MessageBubble> messageBubbles = [];
           for (var message in messages) {
             final messageText = message.data()['text'];
             final messageSender = message.data()['sender'];
-
+            final messageTime = message.data()['time'] as Timestamp;
+            final currentUser = user.email;
             final messageBubble = MessageBubble(
               sender: messageSender,
               text: messageText,
+              time: messageTime,
+              isMe: currentUser == messageSender,
             );
 
             messageBubbles.add(messageBubble);
+            messageBubbles.sort((a , b ) => b.time.compareTo(a.time));
           }
           return Expanded(
             child: ListView(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 10.0, vertical: 20.0),
+              reverse: true,
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
               children: messageBubbles,
             ),
           );
-        }
-        return Container();
+        // return Container();
       },
     );
   }
 }
 
-
 class MessageBubble extends StatelessWidget {
-  MessageBubble({this.sender, this.text});
+  MessageBubble({this.sender, this.text, this.isMe,this.time});
   final String sender;
   final String text;
+  final bool isMe;
+  final Timestamp time;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(
-            sender,
+            ' $sender ${time.toDate()}',
             style: TextStyle(
               fontSize: 12.0,
               color: Colors.black54,
             ),
           ),
           Material(
-            borderRadius: BorderRadius.circular(30.0),
+            borderRadius: isMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  )
+                : BorderRadius.only(
+                    topRight: Radius.circular(30.0),
+                    bottomLeft: Radius.circular(30.0),
+                    bottomRight: Radius.circular(30.0),
+                  ),
             elevation: 8.0,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
               child: Text(
                 text,
                 style: TextStyle(
-                  color: Colors.white,
+                  color: isMe ? Colors.white : Colors.black54,
                   fontSize: 15.0,
                 ),
               ),
